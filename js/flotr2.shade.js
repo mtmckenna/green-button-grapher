610efcfa1ed1e154194b881e641136b2991be73c
Matt McKenna <matt@mtmckenna.com>
2012-05-01 08:00:08 -0700

    cleaned up shade a bit

--- js/flotr2.shade.js
+++ js/flotr2.shade.js
@@ -18,48 +18,30 @@
             var
             ctx = this.ctx,
             options = this.options,
-            grid = options.grid,
-            verticalLines = grid.verticalLines,
-            horizontalLines = grid.horizontalLines,
-            minorVerticalLines = grid.minorVerticalLines,
-            minorHorizontalLines = grid.minorHorizontalLines,
             plotHeight = this.plotHeight,
             plotWidth = this.plotWidth,
-            a, v, i, j;
+            a, i;
 
-            if(verticalLines || minorVerticalLines ||
-            horizontalLines || minorHorizontalLines){
-                //E.fire(this.el, 'flotr:beforegrid', [this.axes.x, this.axes.y, options, this]);
-            }
+            //E.fire(this.el, 'flotr:beforeshade', [this.axes.x, this.axes.y, options, this]);
+
             ctx.save();
-            ctx.lineWidth = 1;
-            ctx.strokeStyle = grid.tickColor;
 
-            ctx.fillStyle = 'black';
-            //ctx.fillRect(55,55,100,100);
-            console.log(options.xaxis.shadeRange[1]);
             var shadeRange = options.xaxis.shadeRange;
             myG = this;
-            ctx.translate(this.plotOffset.left, this.plotOffset.top);
 
             a = this.axes.x;
 
             for (i = 0; i < shadeRange.length; i++) {
                 var pShadeRange = [a.d2p(shadeRange[i][0]),
-                    a.d2p(shadeRange[i][1])];
-                console.log('shaderange: ' + shadeRange[i]);
-                console.log('pshaderange: ' + pShadeRange);
+                a.d2p(shadeRange[i][1])];
                 ctx.fillStyle = "rgba(200, 0, 0, 0.5)";
                 ctx.fillRect(pShadeRange[0], 0,
-                    pShadeRange[1] - pShadeRange[0], plotHeight);
+                pShadeRange[1] - pShadeRange[0], plotHeight);
             }
 
             ctx.restore();
 
-            if(verticalLines || minorVerticalLines ||
-            horizontalLines || minorHorizontalLines){
-                //E.fire(this.el, 'flotr:aftergrid', [this.axes.x, this.axes.y, options, this]);
-            }
+            //E.fire(this.el, 'flotr:aftershade', [this.axes.x, this.axes.y, options, this]);
         }
     });
 
