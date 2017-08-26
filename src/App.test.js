81449f19feaeab3e96fb5dad603c0e5f75c627e8
Matt McKenna <matt@mtmckenna.com>
2017-08-25 17:52:11 -0700

    Add acceptance test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import nightmare from 'nightmare'
+require('nightmare-upload')(nightmare);
+
+import url from 'url'
+import visit from '../helpers/visit'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+describe('When the app boots', function () {
+  test('it displays test data and can parse a new file', async function () {
+    const TEST_FILE_RELATIVE_PATH = '/public/data/Mountain_Single_family_Jan_1_2011_to_Jan_1_2012.xml';
+    const UPDATED_GBD_FILE_PATH = `${process.cwd()}${TEST_FILE_RELATIVE_PATH}`;
+
+    let page = visit('/');
+
+    let initialText = await page
+      .wait(() => document.body.textContent.includes('123 PRETEND ST BERKELEY CA 94707-2701'))
+      .evaluate(() => document.body.textContent);
+
+    expect(initialText).toContain('Total cost: $0.93');
+    expect(initialText).toContain('Cost during peak time: $0.45');
+
+    let textAfterUpload = await page
+      .upload("[type='file']", UPDATED_GBD_FILE_PATH)
+      .wait(() => document.body.textContent.includes('Mountain Single-family'))
+      .evaluate(() => document.body.textContent)
+      .end();
+
+    expect(textAfterUpload).toContain('Total power usage: 16195.04kWh');
+    expect(textAfterUpload).toContain('Total usage during peak time: 6023.79kWh');
+  });
+});
+
+
