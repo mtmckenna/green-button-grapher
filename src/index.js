b613e59c995a7d4f819c2f78a3535c1f252f3c0e
Matt McKenna <matt@mtmckenna.com>
2018-01-28 17:51:38 -0800

    Add GA

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
+import ReactGA from 'react-ga';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+if (process.env.NODE_ENV === 'production') ReactGA.initialize(process.env.REACT_APP_GA_UA);
+
+ReactGA.pageview(window.location.pathname + window.location.search);
