9bcc6d179e994e98029c893bfa3e2bc0dd783b04
Matt McKenna <matt@mtmckenna.com>
2018-02-01 07:45:10 -0800

    Add bugsnag

--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 import ReactGA from 'react-ga';
+import bugsnag from 'bugsnag-js'
+import bugsnagReact from 'bugsnag-react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const bugsnagClient = bugsnag({
+  apiKey: process.env.REACT_APP_BUGSNAG_KEY || 'no-key',
+  notifyReleaseStages: ['production']
+});
+
+const ErrorBoundary = bugsnagClient.use(bugsnagReact(React))
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
 
 if (process.env.NODE_ENV === 'production') ReactGA.initialize(process.env.REACT_APP_GA_UA);
-
 ReactGA.pageview(window.location.pathname + window.location.search);
