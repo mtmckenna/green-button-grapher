7063fef1851d13198dee79ba9049b63c0b625a4e
Matt McKenna <mtmckenna@gmail.com>
2024-05-16 07:47:40 -0700

    put plausible in header

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
-// import registerServiceWorker from './registerServiceWorker';
 import bugsnag from 'bugsnag-js'
 import bugsnagReact from 'bugsnag-react'
 
@@ -20,9 +19,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// registerServiceWorker();
-
-if (process.env.NODE_ENV === 'production') {
-  document.getElementById('root').insertAdjacentHTML('beforeend', '<script defer data-domain="mtmckenna.github.io/green-button-grapher" src="https://plausible.io/js/script.js"></script>');
-}
-// ReactGA.pageview(window.location.pathname + window.location.search);
